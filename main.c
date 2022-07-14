94466d1fd5e79211804340f5db25c0a26dab102c
Chalo1996 <emusyoka759@gmail.com>
2022-07-14 19:02:29 +0000

    The main file of the monty opcode

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+int global_var = 0;
+
+/**
+  * main - executes the commandline arguments
+  * @ac: counts the opcode.
+  * @av: opcodes
+  *
+  * Return: 0
+  */
+
+int main(int ac, char **av)
+{
+	stack_t *stack;
+
+	stack = NULL;
+	if (ac != 2)
+	{
+		printf("USAGE: monty file\n");
+		error_exit(&stack);
+	}
+	file_reader(av[1], &stack);
+	free_dlistint(stack);
+
+	return (0);
+}
