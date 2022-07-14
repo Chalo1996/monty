fed85ad9c1a6c744eb2198121f3b6114bc81d16f
Chalo1996 <emusyoka759@gmail.com>
2022-07-14 19:05:15 +0000

    doesn’t do anything.

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  * _nop - doesn’t do anything.
+  * @stack: pointer to the top of the stack.
+  * @line_number: the index of the current line.
+  *
+  */
+
+void _nop(__attribute__ ((unused))stack_t **stack,
+	  __attribute__ ((unused))unsigned int line_number)
+{
+	;
+}
